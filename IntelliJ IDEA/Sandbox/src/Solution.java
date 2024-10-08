e6e6aecc87fbffb4caa39d8f00524e0ddb6716c9
Marcus Forsberg <marcus.pff03@gmail.com>
2024-10-08 20:37:21 +0200

    changes

--- IntelliJ IDEA/Sandbox/src/Solution.java
+++ IntelliJ IDEA/Sandbox/src/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        for (int i = 1; i < 11; i++) {
+        for (int i = 1; i <= 10; i++) {
 
             System.out.println(number + " x " + i + " = " + number * i);
 
